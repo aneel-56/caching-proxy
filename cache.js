a634b634499661c16c11e2b73773d9e29d20b7ae
aneel tripathee <aneeltripathy56@gmail.com>
2024-10-01 13:27:17 +0545

    Updated

--- cache.js
+++ cache.js
@@ -2,7 +2,7 @@ import redis from "redis";
 import http from "http";
 //connect to the redis client
 const client = redis.createClient({
-  url: "redis://127.0.0.1:6379",
+  url: process.env.RedisUrl,
 });
 
 client.on("error", (err) => console.error("Redis Client Error", err));
